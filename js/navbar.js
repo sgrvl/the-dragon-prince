6ca8ae6facac7c1a64d4ad5b545041332ba6506c
BuildTools <unconfigured@null.spigotmc.org>
2020-01-15 02:38:09 -0500

    Navbar + Pages
    setup la navigation bar, les links et les pages. La recherche marche pas.

---
+++ js/navbar.js
@@ -0,0 +1,16 @@
+window.onscroll = function() {scrollFunction()};
+window.onload = function() {scrollFunction()};
+
+
+function scrollFunction(){
+
+	var logo = document.getElementById("logo");
+
+	if(document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+		logo.style.width = "72%";
+		logo.style.padding = "1rem 2.5rem"
+	} else {
+		logo.style.width = "85%";
+		logo.style.padding = "2rem 2.5rem"
+	}
+};
