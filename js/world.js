06f5d554f3fc8cf0ae2feb0a03327a9b6ffdf7e9
BuildTools <unconfigured@null.spigotmc.org>
2020-01-22 17:35:57 -0500

    map hover info

---
+++ js/world.js
@@ -0,0 +1,123 @@
+var i = 0;
+
+function infoClick1() {
+	if (i == 0) {
+		document.getElementById('info1').style.opacity = '1';
+		i++;
+	} else {
+		document.getElementById('info1').style.opacity = '0';
+		i--;
+	}			
+}
+
+function infoOver1() {
+	document.getElementById('info1').style.opacity = '1';	
+}
+
+function infoOut1() {
+	document.getElementById('info1').style.opacity = '0';
+}
+
+function infoOver2() {
+	document.getElementById('info2').style.opacity = '1';	
+}
+
+function infoOut2() {
+	document.getElementById('info2').style.opacity = '0';
+}
+
+function infoOver3() {
+	document.getElementById('info3').style.opacity = '1';	
+}
+
+function infoOut3() {
+	document.getElementById('info3').style.opacity = '0';
+}
+
+function infoOver4() {
+	document.getElementById('info4').style.opacity = '1';	
+}
+
+function infoOut4() {
+	document.getElementById('info4').style.opacity = '0';
+}
+
+function infoOver5() {
+	document.getElementById('info5').style.opacity = '1';	
+}
+
+function infoOut5() {
+	document.getElementById('info5').style.opacity = '0';
+}
+
+function infoOver6() {
+	document.getElementById('info6').style.opacity = '1';	
+}
+
+function infoOut6() {
+	document.getElementById('info6').style.opacity = '0';
+}
+
+function infoOver7() {
+	document.getElementById('info7').style.opacity = '1';	
+}
+
+function infoOut7() {
+	document.getElementById('info7').style.opacity = '0';
+}
+
+function infoOver8() {
+	document.getElementById('info8').style.opacity = '1';	
+}
+
+function infoOut8() {
+	document.getElementById('info8').style.opacity = '0';
+}
+
+function infoOver9() {
+	document.getElementById('info9').style.opacity = '1';	
+}
+
+function infoOut9() {
+	document.getElementById('info9').style.opacity = '0';
+}
+
+function infoOver10() {
+	document.getElementById('info10').style.opacity = '1';	
+}
+
+function infoOut10() {
+	document.getElementById('info10').style.opacity = '0';
+}
+
+function infoOver11() {
+	document.getElementById('info11').style.opacity = '1';	
+}
+
+function infoOut11() {
+	document.getElementById('info11').style.opacity = '0';
+}
+
+function infoOver12() {
+	document.getElementById('info12').style.opacity = '1';	
+}
+
+function infoOut12() {
+	document.getElementById('info12').style.opacity = '0';
+}
+
+function infoOver13() {
+	document.getElementById('info13').style.opacity = '1';	
+}
+
+function infoOut13() {
+	document.getElementById('info13').style.opacity = '0';
+}
+
+function infoOver14() {
+	document.getElementById('info14').style.opacity = '1';	
+}
+
+function infoOut14() {
+	document.getElementById('info14').style.opacity = '0';
+}
