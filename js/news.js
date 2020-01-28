2506ca7f32273c3c34c6f2dbbd8fd4b5766b97b2
BuildTools <unconfigured@null.spigotmc.org>
2020-01-27 20:29:02 -0500

    news page

--- js/news.js
+++ js/news.js
@@ -1,19 +1,27 @@
 var t;
+var a;
+const slide = document.getElementsByClassName('imgSlide');
+const text = document.getElementsByClassName('sliderButtonsText');
+const title = document.getElementsByClassName('sliderTitle');
+const readM = document.getElementsByClassName('aTitle');
+const load = document.getElementsByClassName('loading');
 
 function stop(){
 	clearTimeout(t);
+	clearTimeout(a);
+	load[0].classList.remove('loadingWidth');
+	load[1].classList.remove('loadingWidth');
+	load[2].classList.remove('loadingWidth');
+	load[3].classList.remove('loadingWidth');
 }
 
-function mySlide(i) {
-	const slide = document.getElementsByClassName('imgSlide');
-	const text = document.getElementsByClassName('sliderButtonsText');
-	const title = document.getElementsByClassName('sliderTitle');
-	const load = document.getElementsByClassName('loading');
+function mySlide(i) {	
 
 	if (i == 0) {
 		text[0].classList.add('sliderActive');
 		slide[0].classList.add('imgOpacity');
 		title[0].classList.add('titleActive');
+		readM[0].classList.add('aTitleActive');
 		load[0].classList.add('loadingWidth');
 		t = setTimeout(function(){
 			mySlide(1);
@@ -24,13 +32,17 @@ function mySlide(i) {
 		text[0].classList.remove('sliderActive');
 		slide[0].classList.remove('imgOpacity');
 		title[0].classList.remove('titleActive');
-		load[0].classList.remove('loadingWidth');
+		readM[0].classList.remove('aTitleActive');
+		a = setTimeout(function(){
+			load[1].classList.remove('loadingWidth');
+		}, 8000);
 	}
 
 	if (i == 1) {
 		text[1].classList.add('sliderActive');
 		slide[1].classList.add('imgOpacity');
 		title[1].classList.add('titleActive');
+		readM[1].classList.add('aTitleActive');
 		load[1].classList.add('loadingWidth');
 		t = setTimeout(function(){
 			mySlide(2);
@@ -41,13 +53,17 @@ function mySlide(i) {
 		text[1].classList.remove('sliderActive');
 		slide[1].classList.remove('imgOpacity');
 		title[1].classList.remove('titleActive');
-		load[1].classList.remove('loadingWidth');
+		readM[1].classList.remove('aTitleActive');	
+		a = setTimeout(function(){
+			load[2].classList.remove('loadingWidth');
+		}, 8000);	
 	}
 
 	if (i == 2) {
 		text[2].classList.add('sliderActive');
 		slide[2].classList.add('imgOpacity');
 		title[2].classList.add('titleActive');
+		readM[2].classList.add('aTitleActive');
 		load[2].classList.add('loadingWidth');
 		t = setTimeout(function(){
 			mySlide(3);
@@ -58,13 +74,17 @@ function mySlide(i) {
 		text[2].classList.remove('sliderActive');
 		slide[2].classList.remove('imgOpacity');
 		title[2].classList.remove('titleActive');
-		load[2].classList.remove('loadingWidth');
+		readM[2].classList.remove('aTitleActive');
+		a = setTimeout(function(){
+			load[3].classList.remove('loadingWidth');
+		}, 8000);
 	}
 
 	if (i == 3) {
 		text[3].classList.add('sliderActive');
 		slide[3].classList.add('imgOpacity');
 		title[3].classList.add('titleActive');
+		readM[3].classList.add('aTitleActive');
 		load[3].classList.add('loadingWidth');
 		t = setTimeout(function(){
 			mySlide(0);
@@ -75,6 +95,9 @@ function mySlide(i) {
 		text[3].classList.remove('sliderActive');
 		slide[3].classList.remove('imgOpacity');
 		title[3].classList.remove('titleActive');
-		load[3].classList.remove('loadingWidth');
+		readM[3].classList.remove('aTitleActive');
+		a = setTimeout(function(){
+			load[0].classList.remove('loadingWidth');
+		}, 8000);
 	}	
 };
